08c9ea0224f77b17b2bf9893f3cec9e009d97bb8
1vanK <1vanK@users.noreply.github.com>
2023-02-11 12:23:25 +0300

    CMake: Сделал универсальной функцию копирования динамических библиотек в папку к экзешнику
    [clear cache]

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -38,3 +38,49 @@ function(dv_set_bin_dir bin_dir)
         set(CMAKE_LIBRARY_OUTPUT_DIRECTORY_${config_name} ${bin_dir} PARENT_SCOPE)
     endforeach()
 endfunction()
+
+# Функция создаёт кастомный таргет, который перед компиляцией экзешника копирует нужные dll или so в папку с экзешником.
+# exe_target_name - таргет, рядом с которым надо положить библиотеки.
+# exe_target_dir - папка, в которую компилируется экзешник.
+# copying_target_name - название создаваемого кастомного таргета, который будет заниматься копированием.
+# Если несколько экзешников компилируются в одну и ту же папку, то у них должны совпадать copying_target_name. 
+# Кастомные таргеты с одинаковыми именами не будут создаваться, и не будет происходить повторного копирования библиотек.
+function(dv_copy_shared_libs_to_bin_dir exe_target_name exe_target_dir copying_target_name)
+    # Кастомный таргет с таким именем ещё не создан
+    if(NOT TARGET ${copying_target_name})
+        # Полные пути к библиотекам, которые будут скопированы в папку с экзешником
+        unset(libs)
+
+        # Если движок скомпилирован как динамическая библиотека, то добавляем её в список
+        if(URHO3D_LIB_TYPE STREQUAL "SHARED")
+            list(APPEND libs "$<TARGET_FILE:Urho3D>")
+        endif()
+
+        # Если движок не линкует MSVC runtime статически, то добавляем библиотеки в список
+        if(MSVC AND NOT dv_static_runtime)
+            # Помимо релизных будут искаться и отладочные версии библиотек
+            set(CMAKE_INSTALL_DEBUG_LIBRARIES TRUE)
+            # Заполняем список CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS
+            include(InstallRequiredSystemLibraries)
+            # Добавляем в список
+            list(APPEND libs ${CMAKE_INSTALL_SYSTEM_RUNTIME_LIBS})
+        endif()
+
+        # Если список пустой, то не создаём кастомный таргет
+        if(NOT libs)
+            return()
+        endif()
+
+        # Сам по себе таргет не активируется. Нужно использовать функцию add_dependencies
+        add_custom_target(${copying_target_name}
+                      COMMAND ${CMAKE_COMMAND} -E copy
+                          ${libs}
+                          "${exe_target_dir}"
+                      COMMENT "Копируем динамические библиотеки в папку ${exe_target_dir}")
+
+        # В IDE кастомный таргет будет отображаться в папке custom_targets
+        set_target_properties(${copying_target_name} PROPERTIES FOLDER "кастомные таргеты")
+    endif()
+
+    add_dependencies(${exe_target_name} ${copying_target_name})
+endfunction()
