4becba9bd206a7bd95ecfba7412e368fbd927bb3
1vanK <1vanK@users.noreply.github.com>
2023-03-29 08:49:17 +0300

    Заменил в convert_models.bat / на \, а то батник не работал

--- bin/convert_models.bat
+++ bin/convert_models.bat
@@ -1,10 +1,10 @@
 @echo off
-set /p ASSETDIR="Enter source assets dir: "
+set /p assetdir="Enter source assets dir: "
 cd /d "%~dp0"
-tool/ogre_importer %ASSETDIR%/Jack.mesh.xml Data/Models/Jack.mdl -t
-tool/ogre_importer %ASSETDIR%/Level.mesh.xml Data/Models/NinjaSnowWar/Level.mdl -t
-tool/ogre_importer %ASSETDIR%/Mushroom.mesh.xml Data/Models/Mushroom.mdl -t
-tool/ogre_importer %ASSETDIR%/Ninja.mesh.xml Data/Models/NinjaSnowWar/Ninja.mdl -t
-tool/ogre_importer %ASSETDIR%/Potion.mesh.xml Data/Models/NinjaSnowWar/Potion.mdl -t
-tool/ogre_importer %ASSETDIR%/SnowBall.mesh.xml Data/Models/NinjaSnowWar/SnowBall.mdl -t
-tool/ogre_importer %ASSETDIR%/SnowCrate.mesh.xml Data/Models/NinjaSnowWar/SnowCrate.mdl -t
+tool\ogre_importer.exe %assetdir%/Jack.mesh.xml Data/Models/Jack.mdl -t
+tool\ogre_importer.exe %assetdir%/Level.mesh.xml Data/Models/NinjaSnowWar/Level.mdl -t
+tool\ogre_importer.exe %assetdir%/Mushroom.mesh.xml Data/Models/Mushroom.mdl -t
+tool\ogre_importer.exe %assetdir%/Ninja.mesh.xml Data/Models/NinjaSnowWar/Ninja.mdl -t
+tool\ogre_importer.exe %assetdir%/Potion.mesh.xml Data/Models/NinjaSnowWar/Potion.mdl -t
+tool\ogre_importer.exe %assetdir%/SnowBall.mesh.xml Data/Models/NinjaSnowWar/SnowBall.mdl -t
+tool\ogre_importer.exe %assetdir%/SnowCrate.mesh.xml Data/Models/NinjaSnowWar/SnowCrate.mdl -t
