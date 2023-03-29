794e93308cf4b86595a3f58dc37432d0af35d420
1vanK <1vanK@users.noreply.github.com>
2023-03-29 08:35:33 +0300

    pbr_demo_*.sh: Релизная сборка имеет приоритет | pbr_demo_*.bat: Запускаем экзешники, а не скрипты

--- bin/pbr_demo_deferred.sh
+++ bin/pbr_demo_deferred.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -f "$(dirname $0)/graph3d_pbr_materials_d" ]; then
-    debug="_d"
-else 
+if [ -f "$(dirname $0)/graph3d_pbr_materials" ]; then
     debug=""
+else 
+    debug="_d"
 fi
 
 $(dirname $0)/graph3d_pbr_materials${debug} -renderpath CoreData/RenderPaths/PBRDeferred.xml $@
--- bin/pbr_demo_deferred_hw_depth.bat
+++ bin/pbr_demo_deferred_hw_depth.bat
@@ -2,5 +2,5 @@
 setlocal
 set "dirname=%~dp0"
 if %0 == "%~dpnx0" where /q "%cd%:%~nx0" && set "dirname=%cd%\"
-if exist "%dirname%Urho3DPlayer.exe" (set "DEBUG=") else (set "DEBUG=_d")
-"%dirname%Urho3DPlayer%DEBUG%" Scripts/42_PBRMaterials.as -renderpath CoreData/RenderPaths/PBRDeferredHWDepth.xml %*
+if exist "%dirname%graph3d_pbr_materials.exe" (set "debug=") else (set "debug=_d")
+"%dirname%graph3d_pbr_materials%debug%.exe" -renderpath CoreData/RenderPaths/PBRDeferredHWDepth.xml %*
