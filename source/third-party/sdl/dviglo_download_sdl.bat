4baa845855370aee7139cb43848386b4fb68413e
1vanK <1vanK@users.noreply.github.com>
2023-04-01 05:39:39 +0300

    Обновил SDL

--- source/third-party/sdl/dviglo_download_sdl.bat
+++ source/third-party/sdl/dviglo_download_sdl.bat
@@ -10,7 +10,7 @@ set "PATH=c:\program files\git\bin"
 git clone https://github.com/libsdl-org/SDL repo
 
 :: Возвращаем состояние репозитория к определённой версии
-git -C repo reset --hard b6ae281e97c4e4680a9010e7e13fe1222c0bcd4b
+git -C repo reset --hard 2aa2fa5449d490159850ba46f9eefbd341edad44
 
 :: Ждём нажатие Enter перед закрытием консоли
 pause
