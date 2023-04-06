3024c1c3f243da8eac9abc8db56b0d6a9273e519
1vanK <1vanK@users.noreply.github.com>
2023-04-06 19:21:18 +0300

    Шейдеры: Удалил квалификатор highp у vShadowPos

--- bin/core_data/shaders/pbr_terrain_blend.glsl
+++ bin/core_data/shaders/pbr_terrain_blend.glsl
@@ -16,11 +16,7 @@ varying vec3 vNormal;
 varying vec4 vWorldPos;
 #ifdef PERPIXEL
     #ifdef SHADOW
-        #ifndef GL_ES
-            varying vec4 vShadowPos[NUMCASCADES];
-        #else
-            varying highp vec4 vShadowPos[NUMCASCADES];
-        #endif
+        varying vec4 vShadowPos[NUMCASCADES];
     #endif
     #ifdef SPOTLIGHT
         varying vec4 vSpotPos;
