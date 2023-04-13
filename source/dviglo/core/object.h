30f3f5fd426eb9f3cc9152c29531f6bd396aa9f6
1vanK <1vanK@users.noreply.github.com>
2023-04-13 13:37:00 +0300

    object.h: В предыдущем коммите забыл удалить IsInstanceOf()

--- source/dviglo/core/object.h
+++ source/dviglo/core/object.h
@@ -69,16 +69,6 @@ public:
 
     /// Return type info static.
     static const TypeInfo* GetTypeInfoStatic() { return nullptr; }
-    /// Check current instance is type of specified type.
-    bool IsInstanceOf(StringHash type) const;
-    /// Check current instance is type of specified type.
-    bool IsInstanceOf(const TypeInfo* typeInfo) const;
-    /// Check current instance is type of specified class.
-    template<typename T> bool IsInstanceOf() const { return IsInstanceOf(T::GetTypeInfoStatic()); }
-    /// Cast the object to specified most derived class.
-    template<typename T> T* Cast() { return IsInstanceOf<T>() ? static_cast<T*>(this) : nullptr; }
-    /// Cast the object to specified most derived class.
-    template<typename T> const T* Cast() const { return IsInstanceOf<T>() ? static_cast<const T*>(this) : nullptr; }
 
     /// Subscribe to an event that can be sent by any sender.
     void subscribe_to_event(StringHash eventType, EventHandler* handler);
