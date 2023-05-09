32b821d23f1cb1de356c67a45317c8388ed7996b
1vanK <1vanK@users.noreply.github.com>
2023-05-09 23:34:38 +0300

    config.h: Стиль

--- source/dviglo/common/config.h
+++ source/dviglo/common/config.h
@@ -1,19 +1,28 @@
-// Copyright (c) 2008-2023 the Urho3D project
 // Copyright (c) 2022-2023 the Dviglo project
+// Copyright (c) 2008-2023 the Urho3D project
 // License: MIT
 
 #pragma once
 
+
 #ifdef DV_SHARED // Динамическая версия движка
-#    if _WIN32 // Visual Studio или MinGW
-#        ifdef DV_IS_BUILDING // Компиляция движка
-#            define DV_API __declspec(dllexport)
-#        else // Использование движка
-#            define DV_API __declspec(dllimport)
-#        endif
-#    else // Linux
-#        define DV_API __attribute__((visibility("default")))
-#    endif
+
+    #ifdef _WIN32 // Visual Studio или MinGW
+
+        #ifdef DV_IS_BUILDING // Компиляция движка
+            #define DV_API __declspec(dllexport)
+        #else // Использование движка
+            #define DV_API __declspec(dllimport)
+        #endif
+
+    #else // Linux
+
+        #define DV_API __attribute__((visibility("default")))
+
+    #endif
+
 #else // Статическая версия движка
-#    define DV_API
-#endif
+
+    #define DV_API
+
+#endif // def DV_SHARED
