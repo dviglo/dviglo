4f89ea73aaccdb81019342dcd2016ef1ac539760
1vanK <1vanK@users.noreply.github.com>
2023-05-09 23:32:14 +0300

    win_wrapped.h: Стиль

--- source/dviglo/common/win_wrapped.h
+++ source/dviglo/common/win_wrapped.h
@@ -1,5 +1,5 @@
-// Copyright (c) 2008-2023 the Urho3D project
 // Copyright (c) 2022-2023 the Dviglo project
+// Copyright (c) 2008-2023 the Urho3D project
 // License: MIT
 
 // Вместо windows.h нужно подключать win_wrapped.h.
@@ -8,15 +8,15 @@
 
 #pragma once
 
+
 #ifdef _WIN32
 
-// В файле rpcndr.h определён тип byte, который конфликтует с byte движка
-#define byte BYTE
+    // В файле rpcndr.h определён тип byte, который конфликтует с byte движка
+    #define byte BYTE
 
-#include <windows.h>
+    #include <windows.h>
 
-#undef byte
-
-#undef max
+    #undef byte
+    #undef max
 
 #endif // def _WIN32
